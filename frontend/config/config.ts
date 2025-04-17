053a87009727cbf9054e130a41f96af917a0a142
luca.fanto <luca.fanto@supsi.ch>
2025-04-17 07:38:05 +0200

    feature:  - deployment on sepolia testnet

--- frontend/config/config.ts
+++ frontend/config/config.ts
@@ -2,7 +2,7 @@ import {ethers} from "ethers";
 import {DocumentControllerApi} from "@dti-isin/backend-api-client"
 import {DocumentCertification__factory} from "../src/typechain-types";
 
-export const contractAddress = `0x5fbdb2315678afecb367f032d93f642f64180aa3`;
+export const contractAddress = `0x9B054a3443803A7aF7d4F5c9250107ab163d7C23`;
 export const provider =  new ethers.JsonRpcProvider(`https://sepolia.infura.io/v3/${import.meta.env.VITE_INFURA_API_KEY}`);
 export const documentCertificationContractNoTX = DocumentCertification__factory.connect(contractAddress, provider);
 
